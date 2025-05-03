1d7977ca7b9f18ac7816b3747aa75d96479b1f48
PATAN EESA MANSOOR <eesamansoor15000@gmail.com>
2025-05-03 21:12:08 +0530

    Update wsgi_server.py

--- Backend/wsgi_server.py
+++ Backend/wsgi_server.py
@@ -1,11 +1,5 @@
 from flask_cors import CORS
 from server import app  # make sure your Flask instance is named `app`
 
-# Apply CORS BEFORE serving
-CORS(app, origins=[
-    "http://localhost:5173",
-    "https://nutrition-estimator-ufmf.onrender.com"
-])
-
 from waitress import serve
 serve(app, host='0.0.0.0', port=5000)
